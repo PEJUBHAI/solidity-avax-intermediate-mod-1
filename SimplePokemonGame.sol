4c15005b5da533af87db46043a28e30450d6f64c
unknown <shawaizjutt978@gmail.com>
2024-07-29 12:12:11 +0500

    Reworked Whole Code

--- SimplePokemonGame.sol
+++ SimplePokemonGame.sol
@@ -1,25 +1,4 @@
-# SimplePokemonGame
-
-A Solidity smart contract for managing a basic Pokemon game. This contract allows you to store and interact with a small team of Pokemon, adjust their trust levels based on the fruit they are fed, and reset them if their trust level falls below zero.
-
-## Description
-
-This Solidity program defines a smart contract called `SimplePokemonGame`. The contract manages a team of three Pokemon, each with a name and a trust level. It includes functions to feed a Pokemon, adjust its trust level based on the type of fruit given, and retrieve the Pokemon's current trust level. If a Pokemon's trust level drops below zero, it is reset.
-
-## Getting Started
-
-### Installing
-
-To run this program, you can use Remix, an online Solidity IDE.
-
-1. Go to the Remix website at [Remix Ethereum](https://remix.ethereum.org/).
-2. Create a new file by clicking on the "+" icon in the left-hand sidebar.
-3. Save the file with a `.sol` extension (e.g., `SimplePokemonGame.sol`).
-
-### Executing program
-
-1. Copy and paste the following code into the file:
- ```// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract SimplePokemonGame {
@@ -95,29 +74,3 @@ contract SimplePokemonGame {
         return true;  // Strings are equal
     }
 }
-```
-2. Compile the code:
-    - Click on the "Solidity Compiler" tab in the left-hand sidebar.
-    - Ensure the "Compiler" option is set to "0.8.0" (or another compatible version).
-    - Click on the "Compile SimplePokemonGame.sol" button.
-
-3. Deploy the contract:
-    - Click on the "Deploy & Run Transactions" tab in the left-hand sidebar.
-    - Select the "SimplePokemonGame" contract from the dropdown menu.
-    - Click on the "Deploy" button.
-
-4. Interact with the deployed contract:
-    - Click on the deployed `SimplePokemonGame` contract in the left-hand sidebar.
-    - Call the `feedPokemon`, `getPokemonTrustLevel`, and other functions as needed.
-
-## Help
-
-If you encounter any issues, make sure your Solidity version is compatible and that you've followed the steps correctly.
-
-## Authors
-
-Shawaiz Islam
-
-## License
-
-This project is licensed under the MIT License - see the LICENSE.md file for details.
